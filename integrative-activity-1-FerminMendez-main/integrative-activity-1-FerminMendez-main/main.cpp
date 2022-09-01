e72da78f8716b103303131305429f87aaa22f777
Fermin Mendez <A01703366@itesm.mx>
2022-09-01 12:18:27 -0500

    add names

--- integrative-activity-1-FerminMendez-main/integrative-activity-1-FerminMendez-main/main.cpp
+++ integrative-activity-1-FerminMendez-main/integrative-activity-1-FerminMendez-main/main.cpp
@@ -1,5 +1,31 @@
 // =========================================================
-// File:
-// Author:
-// Date: 
+// File: main.cpp
+// Authors:
+//  Fermín Méndez García - A01703366
+//  Ricardo Núñez Alánis - A01703259
+// Date: 18/08/2022
 // =========================================================
+
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <algorithm>
+#include <limits.h>
+#include <string>
+
+using namespace std;
+
+
+
+int main(){
+    int n=11;
+    int m=10
+    int *array = new int[];
+
+
+    return 0;
+}
