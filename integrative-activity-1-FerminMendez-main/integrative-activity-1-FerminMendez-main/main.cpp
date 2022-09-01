2d5998aeb652d5cf210ece4cc8886949c822e8bc
Fermin Mendez <A01703366@itesm.mx>
2022-09-01 12:54:36 -0500

    add dp, it doesnt work

--- integrative-activity-1-FerminMendez-main/integrative-activity-1-FerminMendez-main/main.cpp
+++ integrative-activity-1-FerminMendez-main/integrative-activity-1-FerminMendez-main/main.cpp
@@ -19,13 +19,60 @@
 
 using namespace std;
 
+int linear(int m, int n, int x, int y)
+{
+    return y * m + x;
+}
+
+void printArray(int *arr, int n, int m)
+{
+    int count=0;
+    cout << "Array" << endl;
+    for (int i = 0; i < m + 1; i++)
+    {
+        for (int j = 0; j < n + 1; j++)
+        {
+            cout << arr[linear(m, n, i, j)] << " ";
+            count++;
+        }
+        cout << endl;
+        ;
+    }
+    cout<<"Count "<<count;
+}
+
+int DParray(int *arr, int m, int n, string s1, string s2)
+{
+    int sol = 0;
+    // Inizializar el array
+    for (int i = 0; i < m + 1; i++)
+    {
+        arr[linear(m, n, i, 0)] = 1;
+    }
+    for (int i = 0; i < n + 1; i++)
+    {
+        arr[linear(m, n, 0, i)] = 1;
+    }
+
+    return sol;
+}
+
+int main()
+{
+    string s1 = "I I Im got this feeling yeah you know";
+    string s2 = "Ropompompom feeling, one to tri for fai";
+    int m = s1.size();
+    int n = s2.size();
+    int size = (m + 1) * (n + 1);
+    cout<<"m "<<m<<endl;
+    cout<<"n "<<n<<endl;
+    int *array = new int[size];
+    fill(array, array + (m + 1) * (n + 1), 0);
+    int index = DParray(array, m, n, s1, s2);
+    printArray(array, m, n);
 
 
-int main(){
-    int n=11;
-    int m=10
-    int *array = new int[];
-
+    delete[] array;
 
     return 0;
 }
